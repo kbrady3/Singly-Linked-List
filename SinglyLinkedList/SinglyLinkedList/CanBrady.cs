32f965f0dcbd63dae5d59fa4ce39ed63f76c9b22
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-20 15:12:28 -0600

    In progress

---
+++ SinglyLinkedList/SinglyLinkedList/CanBrady.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinglyLinkedList
+{
+    class CanBrady
+    {
+        public string Company { get; set; }
+        public string Content { get; set; }
+        public double Size { get; set; }
+        public double Price { get; set; }
+
+        public CanBrady() { }
+
+        public CanBrady(string company, string content, double size, double price)
+        {
+            Company = company;
+            Content = content;
+            Size = size;
+            Price = price;
+        }
+
+        public void DisplayCan()
+        {
+            string canDetails = "Company: " + Company + ", Content: " + Content + ", Size: " + Size + " ounces, Price: $" + Price;
+            Console.WriteLine(canDetails);
+        }
+    }
+}
